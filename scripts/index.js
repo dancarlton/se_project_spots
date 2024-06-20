588c5fc5ebaedecf867c8aaa65260aae7f35cf94
Dan Carlton <danlouis.carlton@gmail.com>
2024-06-20 12:31:30 -0700

    like buton click event handled

--- scripts/index.js
+++ scripts/index.js
@@ -111,10 +111,15 @@ function getCardElement(data) {
     .cloneNode(true);
   const cardTitle = cardElement.querySelector(".card__title");
   const cardImage = cardElement.querySelector(".card__image");
+  const cardLikeButton = cardElement.querySelector(".card__like-button");
 
   cardTitle.textContent = data.name;
   cardImage.src = data.link;
   cardImage.alt = data.name;
+
+  cardLikeButton.addEventListener("click", () => {
+    cardLikeButton.classList.toggle("card__like-button_liked");
+  });
 
   return cardElement;
 }
