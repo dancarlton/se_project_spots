97355ec6f1d092804e54f28648030c7590f3edd5
Dan Carlton <danlouis.carlton@gmail.com>
2024-06-28 14:17:27 -0700

    error messages toggle on and off

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,14 +1,46 @@
-const checkInputValidity = (inputList, formElement) => {
-  //
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorMessageElement = formElement.querySelector(
+    `#${inputElement.id}-error`
+  );
+  errorMessageElement.textContent = errorMessage;
+  inputElement.classList.add("#modal__input_type_error");
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorMessageElement = formElement.querySelector(
+    `#${inputElement.id}-error`
+  );
+  inputElement.classList.remove("#modal__input_type_error");
+  errorMessageElement.textContent = "";
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add("button_inactive");
+  } else {
+    buttonElement.classList.remove("button_inactive");
+  }
 };
 
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll(".modal__input"));
   const buttonElement = formElement.querySelector(".modal__submit-button");
 
-  console.log(buttonElement);
-  console.log(inputList);
-  // toggleButtonState(inputList, buttonElement);
+  toggleButtonState(inputList, buttonElement);
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", function () {
